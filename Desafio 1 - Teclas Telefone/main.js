e9091bc72db350380164cb07582a604ccf36c19d
Gleysi <83357312+gleysii@users.noreply.github.com>
2023-01-06 19:53:27 -0300

    Update main.js

--- Desafio 1 - Teclas Telefone/main.js
+++ Desafio 1 - Teclas Telefone/main.js
@@ -1,5 +1,13 @@
-document.querySelector('input[type=tel]');
+//selecionando e adicionando nas variáveis
+const listaDeTeclas = document.querySelectorAll('input[type=button]');//valores
+const inputTel = document.querySelector('input[type=tel]');//campo de digitação
 
-const listaDeTeclas = document.querySelectorAll('input[type=button]');
+for (i = 0; i < listaDeTeclas.length; i++){
 
-console.log(listaDeTeclas);
+  const tecla = listaDeTeclas[i];//node list de todos os botões/valores
+  
+  tecla.onclick = function(){//quando a função for disparada
+    inputTel.value = inputTel.value + tecla.value;//o campo recebe o que já está nele mais o valor clicado 
+  }
+  
+}
